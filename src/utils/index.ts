b5be1d6e7a8c7706bafb476baff51baa6be085da
Elmer Bulthuis <elmerbulthuis@gmail.com>
2020-04-16 09:08:03 +0200

    utils

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export * from "./root";
+export * from "./package";
+export * from "./signal";
+export * from "./log";
---
+++ src/utils/package.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs";
+import * as path from "path";
+import { projectRoot } from "./root";
+
+export const packageInfo = readPackageInfo();
+export const packageName = readPackageName();
+
+function readPackageInfo() {
+    const content = fs.readFileSync(path.join(projectRoot, "package.json"), "utf8");
+    return JSON.parse(content);
+}
+
+function readPackageName() {
+    const match = /[^/]*$/.exec(packageInfo.name);
+    if (!match) throw new Error("name not found!");
+    return match.toString();
+}
